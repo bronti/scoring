ea8094dd4ddce2d37ca0e19369581ae2e85ac061
bronti <anya.yaveyn@yandex.ru>
2014-11-12 23:41:49 +0300

    dcg and ndcg

---
+++ ndcg.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from dcg import dcg
+
+def idcg(scores):
+    return dcg(sorted(scores, reverse = True))
+
+def ndcg(scores):
+    return dcg(scores) / idcg(scores)
+
+def simple_test():
+    scores = [2, 3, 1, 0, 0, 2, 0, 2, 0, 1]
+    print(ndcg(scores))
+
+if __name__ == "__main__":
+    simple_test()
+    

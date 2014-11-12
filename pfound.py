07fbecf13e257c47e5594644091af08cc2a021a2
bronti <anya.yaveyn@yandex.ru>
2014-11-13 00:13:28 +0300

    pfound added

---
+++ pfound.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def calc_plook(scores, pbreak):
+    res = [1 - pbreak]
+    for i in range(1, len(scores)):
+        res.append(res[i-1] * (1 - scores[i-1]) * (1 - pbreak))
+    return res
+
+def calc_prel(scores):
+    max_score = max(scores)
+    return [(2 ** scores[i] - 1) / (2 ** max_score) for i in range(len(scores))]
+
+def pfound(scores, pbreak = 0.15):
+    prel = calc_prel(scores)
+    plook = calc_plook(prel, pbreak)
+    summands = [plook[i] * prel[i] for i in range(len(prel))] 
+    return sum(summands)
+
+def simple_test():
+    scores = [2, 3, 1, 0, 0, 2, 0, 2, 0, 1]
+    print(pfound(scores))
+
+if __name__ == "__main__":
+    simple_test()
+        
